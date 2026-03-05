e81c3374e1614ee284365cfe915bae1b330d2187
fragass <fkp9wx6y@gmail.com>
2026-03-04 21:37:03 -0300

    Update dm.create.js

--- api/dm.create.js
+++ api/dm.create.js
@@ -1,5 +1,3 @@
-export const config = { runtime: "nodejs18.x" };
-
 import { supabaseAdmin, requireUser, json } from "./_supabaseAdmin.js";
 
 function dmKey(a, b) {
@@ -71,3 +69,4 @@ export default async function handler(req, res) {
     return json(res, 400, { error: e?.message || "Erro." });
   }
 }
+
